8ab10fb36444ffd0d702d3de791f44a0db66727c
Samuel Wang <swang3068@gatech.edu>
2025-04-08 22:24:22 -0400

    Added sqlfluff config. Test cloud cli.

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -10,11 +9,9 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-
     select 1 as id
     union all
     select null as id
-
 )
 
 select *
